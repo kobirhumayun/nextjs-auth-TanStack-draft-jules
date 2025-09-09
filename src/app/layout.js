91ee20d724bea583b4b4217ce3f7bc234a0f77ec
Humayun <kobirhumayun@gmail.com>
2025-09-09 22:23:31 +0600

    feat: import global css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,22 @@
 // app/layout.jsx
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Home",
+  description: "App home page",
+};
+
 import { Header } from "@/components/Header";
 import Providers from "./providers";
 import { Toaster } from "@/components/ui/sonner";
